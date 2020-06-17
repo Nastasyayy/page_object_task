ce9dec0333278d3bcee1503de5bd116420ba56ae
Nastasyayy <anastasievsk@gmail.com>
2020-06-17 19:06:58 +0300

    Добавлен переход между страницами

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
 class MainPage(BasePage):
     def go_to_login_page(self):
@@ -7,3 +8,14 @@ class MainPage(BasePage):
         login_link.click()
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        #return LoginPage(browser=self.browser, url=self.browser.current_url)
+    def test_guest_can_go_to_login_page(browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
